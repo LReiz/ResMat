a636e780aedc4c0ea76a6eed39b227aaf786f6ac
LReiz <leonardo.reis@gmail.com>
2020-12-03 14:53:29 -0300

    funções da parte 2 do trabalho para plotar gráficos

--- definir_equacao_torcao.m
+++ definir_equacao_torcao.m
@@ -1,6 +1,5 @@
-function [equacao_torcao] = definir_equacao_torcao(equacao_torques, modulo_cisalhamento, momento_inercia)
+function [equacao_torcao] = definir_equacao_torcao(equacao_torques, constante)
   equacao_torcao = [];
-  constante = 1/(modulo_cisalhamento*momento_inercia);
   integral_equacao_torcao = integrar_equacao_degrau(equacao_torques);
   for index = 1 : 1 : size(integral_equacao_torcao)(1)
     equacao_torcao = [equacao_torcao; constante*integral_equacao_torcao(index,:)(1) integral_equacao_torcao(index,:)(2) integral_equacao_torcao(index,:)(3)];
